3034aae03372bbca1c2543d7fc5932eee204eae8
PubuduAbeywickrama <pubudutharaka6@gmail.com>
2023-04-26 20:18:58 +0530

    Initial commit

---
+++ app/src/main/java/com/example/signuplogin/ProfileActivity.java
@@ -0,0 +1,93 @@
+package com.example.signuplogin;
+
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
+
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.Query;
+import com.google.firebase.database.ValueEventListener;
+
+public class ProfileActivity extends AppCompatActivity {
+
+    TextView profileName, profileEmail, profileUsername, profilePasssword;
+    TextView titleName, titleUsername;
+    Button editProfile;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_profile);
+
+        profileName = findViewById(R.id.profileName);
+        profileEmail = findViewById(R.id.profileEmail);
+        profileUsername = findViewById(R.id.profileUsername);
+        profilePasssword = findViewById(R.id.profilePassword);
+        titleName = findViewById(R.id.titleName);
+        titleUsername = findViewById(R.id.titleUserName);
+        editProfile = findViewById(R.id.btnEdit);
+
+        showUserData();
+
+        editProfile.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                passUserData();
+            }
+        });
+    }
+
+    public void showUserData(){
+        Intent intent = getIntent();
+
+        titleName.setText(intent.getStringExtra("name"));
+        titleUsername.setText(intent.getStringExtra("username"));
+
+        profileName.setText(intent.getStringExtra("name"));
+        profileUsername.setText(intent.getStringExtra("username"));
+        profileEmail.setText(intent.getStringExtra("email"));
+        profilePasssword.setText(intent.getStringExtra("password"));
+    }
+
+    public void passUserData(){
+        String userUsername = profileUsername.getText().toString().trim();
+
+        DatabaseReference reference = FirebaseDatabase.getInstance().getReference("users");
+        Query checkUserDatabase = reference.orderByChild("username").equalTo(userUsername);
+
+        checkUserDatabase.addListenerForSingleValueEvent(new ValueEventListener() {
+            @Override
+            public void onDataChange(@NonNull DataSnapshot snapshot) {
+                if(snapshot.exists()){
+                    String nameFromDB = snapshot.child(userUsername).child("name").getValue(String.class);
+                    String emailFromDB = snapshot.child(userUsername).child("email").getValue(String.class);
+                    String usernameFromDB = snapshot.child(userUsername).child("username").getValue(String.class);
+                    String passwordFromDB = snapshot.child(userUsername).child("password").getValue(String.class);
+
+                    Intent intent = new Intent(ProfileActivity.this, EditProfileActivity.class);
+
+                    intent.putExtra("name", nameFromDB);
+                    intent.putExtra("email", emailFromDB);
+                    intent.putExtra("username", usernameFromDB);
+                    intent.putExtra("password", passwordFromDB);
+
+                    startActivity(intent);
+                }
+            }
+
+            @Override
+            public void onCancelled(@NonNull DatabaseError error) {
+
+            }
+        });
+    }
+
+}
